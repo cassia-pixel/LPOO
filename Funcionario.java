956b575730d92f355059ee733c787548e073c6ba
cassia-pixel <cassiasillva.r0sa@gmail.com>
2020-03-05 12:04:26 -0400

    exercicios

---
+++ Funcionario.java
@@ -0,0 +1,16 @@
+public class Funcionario {
+    String nome;
+    String departamento; 
+    double salario;
+    String entradaBanco;
+    String rg; 
+
+    void recebeAumento(int x) {
+
+    }
+    
+    void calculaGanhoAnual() {
+
+    }
+
+}
---
+++ Modelo.java
@@ -0,0 +1,7 @@
+public class Modelo {
+    public static void main() {
+
+        Funcionario func1 = new Funcionario();
+        
+    }
+}
